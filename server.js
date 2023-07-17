ecb5963be868287da1c0652e62c06020c49df5a1
Moiz Shahid <82671962+moizshd@users.noreply.github.com>
2023-07-18 00:09:35 +0500

    second commit

--- server.js
+++ server.js
@@ -1,89 +1,140 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const speech = require('@google-cloud/speech');
-const socketIo = require('socket.io');
-const cors = require('cors');
-process.env["GOOGLE_APPLICATION_CREDENTIALS"] = "D:\\Projects\\google-speech-to-text\\speech-server\\google-credentials.json";
+const express = require("express");
+const speech = require("@google-cloud/speech");
+
+const logger = require("morgan");
+
+const bodyParser = require("body-parser");
+
+const cors = require("cors");
+
+const http = require("http");
+const { Server } = require("socket.io");
 
 const app = express();
-const corsOptions = {
-    origin: '*', // Allows all origins
-    optionsSuccessStatus: 200 // For legacy browser support
-};
-app.use(cors(corsOptions));
+
+app.use(cors());
+app.use(logger("dev"));
+
 app.use(bodyParser.json());
 
-const server = require('http').Server(app);
-const io = socketIo(server, {
+const server = http.createServer(app);
+
+const io = new Server(server, {
     cors: {
         origin: "http://localhost:3000",
         methods: ["GET", "POST"],
-        credentials: true
-    }
+    },
 });
-const client = new speech.SpeechClient();
 
-io.on('connection', (socket) => {
-    console.log('Client connected');
-    // socket.on('streamAudio', async (data) => {
-    //     console.log('Received audio stream from client');  // Add this
-    //     const audioBytes = data.audio;
-    //     const request = {
-    //         audio: {
-    //             content: audioBytes,
-    //         },
-    //         config: {
-    //             encoding: 'LINEAR16',
-    //             sampleRateHertz: 16000,
-    //             languageCode: 'en-US',
-    //         },
-    //     };
+process.env.GOOGLE_APPLICATION_CREDENTIALS = "./google-credentials.json";
 
-    //     try {
-    //         const [response] = await client.recognize(request);
-    //         const transcription = response.results
-    //             .map(result => result.alternatives[0].transcript)
-    //             .join('\n');
+const speechClient = new speech.SpeechClient();
 
-    //         socket.emit('transcription', transcription);
-    //     } catch (e) {
-    //         console.error('Error transcribing audio:', e);
-    //     }
-    // });
-    socket.on('streamAudio', async (data) => {
-        console.log('Received audio stream from client');
-        const audioBytes = data.audio;
-        const request = {
-            audio: {
-                content: audioBytes,
-            },
-            config: {
-                encoding: 'LINEAR16',
-                sampleRateHertz: 48000, // Adjusted to match the WAV header
-                languageCode: 'en-US',
-            },
-        };
+io.on("connection", (socket) => {
+    let recognizeStream = null;
+    console.log("** a user connected - " + socket.id + " **\n");
 
-        try {
-            const [response] = await client.recognize(request);
-            const transcription = response.results
-                .map(result => result.alternatives[0].transcript)
-                .join('\n');
+    socket.on("disconnect", () => {
+        console.log("** user disconnected ** \n");
+    });
 
-            console.log(`Transcription: ${transcription}`);  // Add this
+    socket.on("send_message", (message) => {
+        console.log("message: " + message);
+        setTimeout(() => {
+            io.emit("receive_message", "got this message" + message);
+        }, 1000);
+    });
 
-            socket.emit('transcription', transcription);
-        } catch (e) {
-            console.error('Error transcribing audio:', e);
+    socket.on("startGoogleCloudStream", function (data) {
+        startRecognitionStream(this, data);
+    });
+
+    socket.on("endGoogleCloudStream", function () {
+        console.log("** ending google cloud stream **\n");
+        stopRecognitionStream();
+    });
+
+    socket.on("send_audio_data", async (audioData) => {
+        io.emit("receive_message", "Got audio data");
+        if (recognizeStream !== null) {
+            try {
+                recognizeStream.write(audioData.audio);
+            } catch (err) {
+                console.log("Error calling google api " + err);
+            }
+        } else {
+            console.log("RecognizeStream is null");
         }
     });
 
+    function startRecognitionStream(client) {
+        console.log("* StartRecognitionStream\n");
+        try {
+            recognizeStream = speechClient
+                .streamingRecognize(request)
+                .on("error", console.error)
+                .on("data", (data) => {
+                    const result = data.results[0];
+                    const isFinal = result.isFinal;
+
+                    const transcription = data.results
+                        .map((result) => result.alternatives[0].transcript)
+                        .join("\n");
+
+                    console.log(`Transcription: `, transcription);
+
+                    client.emit("receive_audio_text", {
+                        text: transcription,
+                        isFinal: isFinal,
+                    });
+
+                    // if end of utterance, let's restart stream
+                    // this is a small hack to keep restarting the stream on the server and keep the connection with Google api
+                    // Google api disconects the stream every five minutes
+                    if (data.results[0] && data.results[0].isFinal) {
+                        stopRecognitionStream();
+                        startRecognitionStream(client);
+                        console.log("restarted stream serverside");
+                    }
+                });
+        } catch (err) {
+            console.error("Error streaming google api " + err);
+        }
+    }
+
+    function stopRecognitionStream() {
+        if (recognizeStream) {
+            console.log("* StopRecognitionStream \n");
+            recognizeStream.end();
+        }
+        recognizeStream = null;
+    }
 });
 
-try {
-    server.listen(4000, () => {
-        console.log('Server listening on port 4000');
-    });
-} catch (error) {
-    console.error('Error starting server:', error);
-}
+server.listen(8081, () => {
+    console.log("WebSocket server listening on port 8081.");
+});
+
+
+const encoding = "LINEAR16";
+const sampleRateHertz = 16000;
+const languageCode = "ko-KR"; //en-US
+const alternativeLanguageCodes = ["en-US", "ko-KR"];
+
+const request = {
+    config: {
+        encoding: encoding,
+        sampleRateHertz: sampleRateHertz,
+        languageCode: "en-US",
+        enableWordTimeOffsets: true,
+        enableAutomaticPunctuation: true,
+        enableWordConfidence: true,
+        enableSpeakerDiarization: true,
+        //diarizationSpeakerCount: 2,
+        //model: "video",
+        model: "command_and_search",
+        //model: "default",
+        useEnhanced: true,
+    },
+    interimResults: true,
+};
